40065ba4c9dbf6ee1c681c27078f3774c1c25bf2
RobertRuta <robert@ruta.co.uk>
2022-04-26 16:52:51 +0200

    Created basic euler method solver in solver.py.

--- solver.py
+++ solver.py
@@ -1,20 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from numpy import sin, cos, pi
+
 def deriv(t,y):
     theta = y[0]
     thetadot = y[1]
 
-    q = 1.15
+
     
+    thetaddot = -thetadot/q - sin(theta) + g*cos(w*t)
+    
+    return np.array([thetadot, thetaddot])
 
-    thetaddot = -thetadot/q
+# PARAMS
+q = 2
+g = 1.5
+w = 2/3
 
-    return np.array(thetadot, thetaddot)
-
-steps = 10**8
+steps = 10**7
 t_0 = 0
 t_f = 10
+dt = 2*pi/w
 
-T = np.linspace(t_0, t_f, steps)
-print("number of bytes occupied by T: ", T.nbytes)
+#INIT CONDITIONS
+y_0 = np.array([0,0])
+
+t = t_0
+y = y_0
+Y = np.empty([steps,2])
+
+for step in range(steps):
+    
+    dy = deriv(t,y)*dt
+    y = y + dy
+    Y[step,0] = y[0]
+    Y[step,1] = y[1]
+    
+    t += dt
+    print("step: ", step)
+    
+print("Y array size: ", Y.nbytes)
+    
+plt.scatter(Y[:,0],Y[:,1])
+plt.show()
